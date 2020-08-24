1ec9b94e37296442f1e3df0fca2e226314d802bb
George Antoniadis <g.antoniadis@paymentcomponents.com>
2020-08-24 16:07:16 +0300

    39

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I am only called Once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -21,5 +25,20 @@ class GreetingTest {
     @Test
     void helloWorld1() {
         System.out.println(greeting.helloWorld("George"));
+    }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each....");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called Once!!!");
     }
 }
